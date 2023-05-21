c66619a11c259e6c659abc0281b9fd9a11f46d13
Ridoy Sharif <developersridoy@gmail.com>
2023-05-21 17:43:19 +0600

    Code revision

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { config } = require("dotenv");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const { config } = require("dotenv");
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2v9b72.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     const toyMarketplaceCollection = client
       .db("toyMarketplaceDB")
@@ -73,7 +74,6 @@ async function run() {
           .toArray();
       }
       res.send(result);
-      console.log(result);
     });
 
     //specific user specific data using email
